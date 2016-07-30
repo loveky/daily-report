60440cfde9a264fca9ca39968949a4002b23ccde
loveky <ylzcylx@gmail.com>
2016-07-30 19:18:21 +0800

    支持打开邮件客户端发送

--- src/app.js
+++ src/app.js
@@ -4,14 +4,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import dataService from './dataService';
 import currentDate from './currentDate';
+import Clipboard from 'clipboard';
 
 let todayRelease = [];
 let todayTask = [];
 
-$('.today').text(currentDate.replace(/(\d\d\d\d)(\d\d)(\d\d)/, '$1-$2-$3'));
+$('.today').text(currentDate);
+
+function refPathOfToday(path) {
+    return 'reports/' + currentDate.replace(/-/g, '') + '/' + path;
+}
 
 // 监听当天任务列表变化
-dataService.ref(`reports/${currentDate}/tasks`).on('value', (snapshot) => {
+dataService.ref(refPathOfToday('tasks')).on('value', (snapshot) => {
     todayRelease = [];
     todayTask = [];
     const $todayRelase = $('.today-release');
@@ -39,12 +44,12 @@ dataService.ref(`reports/${currentDate}/tasks`).on('value', (snapshot) => {
 
     index = 1;
     for (let task of todayTask) {
-        $todayTask.append(`<p class="task">${index++}. ${task.taskContent} ${task.taskProgress}%</p>`)
+        $todayTask.append(`<p class="task">${index++}. ${task.taskContent} ${task.taskProgress > 0 ? task.taskProgress + '%' : ''}</p>`)
     }
 });
 
 // 监听当天已经提交任务的人员列表
-dataService.ref(`reports/${currentDate}/reporters`).on('value', (snapshot) => {
+dataService.ref(refPathOfToday('repoteres')).on('value', (snapshot) => {
 
 });
 
@@ -64,7 +69,19 @@ $('.submit-task').on('click', function () {
         const taskRelease = $task.find('.release-task').is(':checked');
         
         if (taskContent.length > 0) {
-            dataService.ref(`reports/${currentDate}/tasks`).push({taskContent, taskProgress, taskRelease})
+            dataService.ref(refPathOfToday('tasks')).push({taskContent, taskProgress, taskRelease})
         }
     });
+});
+
+const clipboard = new Clipboard('.send-report');
+$('.send-report').tooltip({
+    container: 'body',
+    html: true,
+    trigger: 'manual',
+    title: '日报内容已复制至剪切板，请<a href="mailto:fe-rd1@jd.com?subject=前端研发一组' + currentDate + '早会记要">打开邮件客户端</a>粘贴后发送～'
+})
+clipboard.on('success', () => {
+    $('.send-report').tooltip('show');
+    setTimeout(() => {$('.send-report').tooltip('hide')}, 3000)
 });
--- src/currentDate.js
+++ src/currentDate.js
@@ -1,3 +1,3 @@
 let time = new Date();
 
-export default [time.getFullYear(), (time.getMonth() < 9 ? '0' : '' ), time.getMonth(), (time.getDate() < 9 ? '0' : '' ), time.getDate()].join('');
+export default [time.getFullYear(), '-', (time.getMonth() + 1 < 9 ? '0' : '' ), time.getMonth() + 1, '-', (time.getDate() < 9 ? '0' : '' ), time.getDate()].join('');
